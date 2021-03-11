55f078d08a89f54cb258d9c591ecf18e1f9147d1
DBankx <61208493+DBankx@users.noreply.github.com>
2021-03-11 08:15:22 +0000

    Heroku

--- src/app.ts
+++ src/app.ts
@@ -103,10 +103,10 @@ class App {
 
   private deployApplication(){
     if(this.env === "production"){
-      this.app.use(express.static(path.join(__dirname, "../client/build")));
+      this.app.use(express.static(path.join(__dirname, "client")));
 
       this.app.get("*", (req: Request, res: Response) =>
-        res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"))
+        res.sendFile(path.resolve(__dirname, "client", "index.html"))
       );
     } else {
       this.app.get("/", (req: Request, res: Response) => res.send("server is running"))

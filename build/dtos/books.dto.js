88ddb5ae1804b2b382b7e8328a3ff1baaab87a4f
DBankx <61208493+DBankx@users.noreply.github.com>
2021-03-11 02:39:02 +0000

    last

--- build/dtos/books.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BookCreateDto = void 0;
-class BookCreateDto {
-}
-exports.BookCreateDto = BookCreateDto;
-//# sourceMappingURL=books.dto.js.map
